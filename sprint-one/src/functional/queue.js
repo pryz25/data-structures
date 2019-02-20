895198568aef6881f0c5d885f6bf30c789dc188e
Peter R <pryzanych@gmail.com>
2019-02-20 14:54:05 -0800

    First commit to solo

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,32 @@
 var Queue = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
-
+  var count = 0;
+  var minCount = 0;
   someInstance.enqueue = function(value) {
+    // Look at list of keys
+    // New key is +1 last key
+    // put value under key
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    var output = storage[minCount];
+    if (count - minCount === 0) {
+      return undefined;
+    } else {
+      delete storage[minCount];
+      minCount++;
+      return output;
+    }
   };
 
   someInstance.size = function() {
+    return count - minCount;
   };
 
   return someInstance;
