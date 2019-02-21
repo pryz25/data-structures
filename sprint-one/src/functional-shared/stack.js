1232833cf9097a33d397a0186e82e8d1928e4866
Peter R <pryzanych@gmail.com>
2019-02-20 21:21:55 -0800

    Functional-shared Complete

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.count = 0; // always have a count
+  newStack.storage = {}; // always have storage for variables
+   _.extend(newStack, stackMethods); // extend methods into newStack
+  
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push(value) { // push has value
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  pop() {
+    if (this.count === 0) { // check if count is 0
+      return undefined;
+    } else {
+      this.count--; // move one level down
+      var output = this.storage[this.count]; // store values to be deleted
+      delete this.storage[this.count]; // delete
+      return output; // return deleted value
+    }
+  },
+  size() {
+    return this.count;
+  }  
+};
+
+
+
+
+
+
 
 
