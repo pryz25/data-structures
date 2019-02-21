16db61e762e66d9e203ad27259594c4e2e0d888e
Peter R <pryzanych@gmail.com>
2019-02-21 10:20:29 -0800

    Commit test

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,7 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    
   }
 
 }
