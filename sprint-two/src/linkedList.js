87b35acf74b5f30f207022f10a1da9cc1413cb6f
pryz25 <pryzanych@gmail.com>
2019-02-21 15:57:24 -0800

    linkedList can addToTail and removeHead

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,11 +2,26 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.current = null;
 
   list.addToTail = function(value) {
+    list.current = Node(value);// put value into Node constructor
+    if (list.head === null) {// check if head is empty
+      list.head = list.current;// if empty add to head rather than tail
+      list.tail = list.current; // set both pointers to head
+    } else {
+      list.tail.next = list.current;// previous tail points to current node
+      list.tail = list.current;// change tail pointer to current node
+    }
   };
 
   list.removeHead = function() {
+    // declare a variable, assign list.head.value to it
+    var output = list.head.value;
+    // reassign list.head to list.head.next
+    list.head = list.head.next;
+    // return variable
+    return output;
   };
 
   list.contains = function(target) {
