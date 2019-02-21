c052428e42edab464df86e22d8128ee03b28783f
Peter R <pryzanych@gmail.com>
2019-02-20 20:11:13 -0800

    Functional Complete

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
+  // var minCount = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+      if (count === 0) { // if there are no values return undefined
+        return undefined;
+      }
+      count--;
+      var output = storage[count]; // store the to be deleted value
+      delete storage[count]; // if there are values delete it
+    //}
+    return output;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
