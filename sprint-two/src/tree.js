2244a02889811c9c05336e4aeb24507df621c3d2
pryz25 <pryzanych@gmail.com>
2019-02-22 11:19:57 -0800

    Pass all Tree tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,30 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  
+  _.extend (newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var tree = Tree(value);// create new Tree with value
+  this.children.push(tree);// push new Tree with this operator
+
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {// check target against first tree
+     return true;// if match then return true
+  } else if (this.children.length > 0) {// else check if any children
+    for (var i = 0; i < this.children.length; i++) { // if none then return false
+      if (this.children[i].contains(target)) {
+        return true;
+      }; // if yes iterate contains over length
+    }
+  } 
+return false;
 };
 
 
