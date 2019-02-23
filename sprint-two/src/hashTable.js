e457a924fe03687b2893d90a10bad6b941d06534
pryz25 <pryzanych@gmail.com>
2019-02-23 10:19:09 -0800

    Fixed collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,27 +7,29 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage.get(index)) {
-    
+  if (this._storage.get(index) === undefined) { // if there's nothing in index
+    this._storage.set(index, {}); // create object 
+    this._storage.get(index)[k] = v; // with k-v pair k:v
   } else {  
-    this._storage.set(index, v); // set key value pair to index
+    this._storage.get(index)[k] = v; // add k-v pair to index object
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index); // get value that's at the storage index and return it
+  return this._storage.get(index)[k]; // get value that's at the storage index and return it
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined); // delete the value at out given index in our storage array
+  delete this._storage.get(index)[k]; // delete the value at out given index in our storage array
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * 
  */
 
 
