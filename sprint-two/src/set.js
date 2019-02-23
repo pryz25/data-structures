6ec40012cd30bc349c99aac6e2a540319ccafa2d
pryz25 <pryzanych@gmail.com>
2019-02-22 20:49:33 -0800

    Sets are functional

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; 
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item; // declare property and value as item
 };
 
 setPrototype.contains = function(item) {
+  for(let key in this._storage) {// for every item in storage
+    if (key === item) {// if the item is found...
+      return true; // return true
+    }
+  }
+  return false; // return false
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
